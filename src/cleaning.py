f343efe740967b38cafcd45a5bec07e1d77215f4
Silvia Hernandez <silviaherf@gmail.com>
2020-09-11 17:07:43 +0200

    csv abierto

--- src/cleaning.py
+++ src/cleaning.py
@@ -6,8 +6,9 @@ def open_cvs():
     This function opens a cvs file from src folder in the project, in a shape of a pandas DataFrame
     No parameters are needed, except of the name and the folder the function will ask you
     """
-    ##folder=input("Please, enter the folder where your cvs file is saved:\n")
+    folder=input("Please, enter the folder where your cvs file is saved:\n")
     file=input('Please, enter the name for the cvs file you want to open without extension:\n')
 
-    return pd.read_csv(f'{file}.csv')
+    return pd.read_csv(f'../{folder}/{file}.csv')
+
 
