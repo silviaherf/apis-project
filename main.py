feccd17bba10889e3a65e7aa2915918e466208b3
Silvia Hernandez <silviaherf@gmail.com>
2020-09-12 18:30:29 +0200

    main.py limpiado y pasadas funciones a manipulating

--- main.py
+++ main.py
@@ -1,79 +1,14 @@
  #!/usr/bin/env python3
 import sys
-import argparse
 import pandas as pd
 import src.cleaning as clean
 import src.manipulating  as man
 
 
-def onlyYears(minYear=1902, maxYear=2020):
-    def wrapper(yearStr):
-        year = 2020
-        try:
-            year = int(yearStr)
-        except Exception:
-            raise argparse.ArgumentTypeError(f"{yearStr} is an invalid positive int value")
-        
-        if year >= minYear and year <= maxYear:
-            return year
-        else:
-            raise argparse.ArgumentTypeError(f"year must be between {minYear} and {maxYear}")
-    return wrapper
-
-def rangeAge(ages):    
-    def wrapper(age):    
-        if age in ages:
-            return age
-        else:
-            raise argparse.ArgumentTypeError(f"Age parameter must be one out of the folowing ones: {ages}")
-    return wrapper
-
 def main():
-    df=pd.read_csv('src/movies.csv',encoding='latin-1')
-    ages=df['Age'].unique()
-    minYear = df["Year"].min()
-    maxYear = df["Year"].max()
+    man.get_dataset()
 
-    parser = argparse.ArgumentParser(description='Filter the imported dataset by selected values')
-    parser.add_argument('-y', dest='year',
-                        default=2015,       
-                        type=onlyYears(minYear,maxYear),
-                        help="Selected year")
-    
-    parser.add_argument('-l', dest='language',             
-                        type=str,
-                        help="Selected director")
 
-    
-    parser.add_argument('-a', dest='age',
-                        type=rangeAge(ages),
-                        help="Selected age recommendation")
-
-    parser.add_argument('-c', dest='country',  
-                        default='United States',
-                        type=str,
-                        help="Selected country")
-                      
-
-    args = parser.parse_args()
-   
-    if args.age is None:
-        if args.language is None:
-            print(df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].head())
-            cuenta=df[(df.Year==args.year)  & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].index.value_counts().sum()
-
-        else:
-            print(df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].head())
-            cuenta=df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].index.value_counts().sum()
-    else:
-        if args.language is None:
-            print(df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].head())
-            cuenta=df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].index.value_counts().sum()
-        else:
-            print(df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].head())
-            cuenta=df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].index.value_counts().sum()
-    
-    return print(f'{cuenta} movies matched with those filters')
 
 if __name__ == "__main__":
     main()
--- src/manipulating.py
+++ src/manipulating.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import os
+import argparse
+import src.cleaning as clean
+import requests
+from dotenv import load_dotenv
+from pathlib import Path  
+env_path = Path('/src') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 def open_csv():
 
@@ -25,4 +33,95 @@ def export_csv(df):
     name=input('Please, enter the name for the cvs file you want to export')
     df.to_csv(f"{name}.csv",index = False)
     return f"{name} has been exported to cvs file"
+
+
+
+
+def get_dataset():
+    df=pd.read_csv('src/movies.csv',encoding='latin-1')
+    ages=df['Age'].unique()
+    minYear = df["Year"].min()
+    maxYear = df["Year"].max()
+
+    parser = argparse.ArgumentParser(description='Filter the imported dataset by selected values')
+    parser.add_argument('-y', dest='year',
+                        default=2015,       
+                        type=onlyYears(minYear,maxYear),
+                        help="Selected year")
+    
+    parser.add_argument('-l', dest='language',             
+                        type=str,
+                        help="Selected director")
+
+    
+    parser.add_argument('-a', dest='age',
+                        type=rangeAge(ages),
+                        help="Selected age recommendation")
+
+    parser.add_argument('-c', dest='country',  
+                        default='United States',
+                        type=str,
+                        help="Selected country")
+                      
+
+    args = parser.parse_args()
+   
+    if args.age is None:
+        if args.language is None:
+            print(df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].head())
+            cuenta=df[(df.Year==args.year)  & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].index.value_counts().sum()
+
+        else:
+            print(df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].head())
+            cuenta=df[(df.Year==args.year) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].index.value_counts().sum()
+    else:
+        if args.language is None:
+            print(df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].head())
+            cuenta=df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}', regex= True, na=False))].index.value_counts().sum()
+        else:
+            print(df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].head())
+            cuenta=df[(df.Year==args.year) & (df.Age==args.age) & (df.Country.str.contains(f'{args.country}')) & (df.Language.str.contains(f'{args.language}', regex= True, na=False))].index.value_counts().sum()
+    
+    return print(f'{cuenta} movies matched with those filters')
+
+
+def onlyYears(minYear=1902, maxYear=2020):
+    def wrapper(yearStr):
+        year = 2020
+        try:
+            year = int(yearStr)
+        except Exception:
+            raise argparse.ArgumentTypeError(f"{yearStr} is an invalid positive int value")
+        
+        if year >= minYear and year <= maxYear:
+            return year
+        else:
+            raise argparse.ArgumentTypeError(f"year must be between {minYear} and {maxYear}")
+    return wrapper
+
+def rangeAge(ages):    
+    def wrapper(age):    
+        if age in ages:
+            return age
+        else:
+            raise argparse.ArgumentTypeError(f"Age parameter must be one out of the folowing ones: {ages}")
+    return wrapper
+
+def getUrl(query,api_key=os.getenv('NYT_APIKEY')):
+    """
+    This function gets information out of an API 
+
+    """
+
+    baseUrl='https://api.nytimes.com/svc/movies/v2/reviews'
+    url = f"{baseUrl}/search.json?query={query}&api-key={api_key}"
+    
+    requestHeaders = {
+    "Accept": "application/json"
+  }
+
+    response = requests.get(url, headers=requestHeaders)
+    print(f"Requested data; status_code:{response.status_code}")
+    data=response.json()
+    return data
 
