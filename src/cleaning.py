2151ad23b9da248f65b9ce3e0f16cb4694e749bd
Silvia Hernandez <silviaherf@gmail.com>
2020-09-12 09:33:21 +0200

    first dataset cleaned up and exported to csv file

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import regex as re
 
-def open_cvs():
+def open_csv():
 
     """
     This function opens a cvs file from a folder in the project, in a shape of a pandas DataFrame
@@ -26,6 +26,18 @@ def rename_columns(df):
         actual_col[column]=column.replace(r" ","_")
     df=df.rename(columns = actual_col, inplace = True)
     return df
+
+def drop_columns(df,*columns):
+    """ 
+    This function drop from the DataFrame the selected columns.
+    The arguments needed are 
+    df:the DataFrame 
+    columns*:the columns we want to drop. It accept columns as needed; just enter them as string separated by comma ","
+    """
+    for column in columns:
+        df=df.drop(column,axis=1)
+    return df
+
 
 def rounding(df):
     """
@@ -65,3 +77,13 @@ def convert_integer(df,column):
     """
     df[column]=df[column].map(lambda x: int(x))
     return df   
+
+def export_csv(df):
+    """ 
+    This function export a cvs file to src folder in the project.
+    To use it, it needs one parameter:
+    df:the DataFrame we are willing to export to cvs
+    """
+    name=input('Please, enter the name for the cvs file you want to export')
+    df.to_csv(f"{name}.csv",index = False)
+    return f"{name} has been exported to cvs file"
