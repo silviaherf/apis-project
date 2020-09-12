09abeb7f2fee4c34f5d7100cf7926e0ee8298aa3
Silvia Hernandez <silviaherf@gmail.com>
2020-09-12 08:57:43 +0200

    adding functions to celaning.py

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import regex as re
 
 def open_cvs():
 
@@ -12,4 +13,55 @@ def open_cvs():
     return pd.read_csv(f'../{folder}/{file}.csv')
 
 
-def get_url(endpoint):
+#def get_url(endpoint):
+
+
+def rename_columns(df):
+    """ 
+    This function checks the name of the columns in a DataFrame and replace blank spaces with "_".
+    The only argument needed is df:the DataFrame
+    """
+    actual_col={}
+    for column in df.columns:
+        actual_col[column]=column.replace(r" ","_")
+    df=df.rename(columns = actual_col, inplace = True)
+    return df
+
+def rounding(df):
+    """
+    This function rounds every numerical column in a DataFrame to 2 decimals.
+    The only argument needed is df:the DataFrame
+    """
+    for column in df.select_dtypes(include=['int','float']).columns:
+        df[column]=df[column].map(lambda x:round(x,2))
+    return df
+
+def rounding_value(num):
+    """
+    This function rounds a numerical value to 2 decimals.
+    The only argument needed is num:the value we want to round
+    """
+
+    return  round(num,2)
+
+def white_spaces(df,column):
+    """
+    This function removes every white space in the selected column in a DataFrame.
+    The two arguments needed are:
+    df:the DataFrame
+    column:the column from the dataFrame as a string
+    """
+
+    df[column]=df[column].map(lambda x: "".join(x.split()))
+
+    return df   
+
+def convert_integer(df,column):
+    """
+    This function changes the type of values of a column in a DataFrame to integer.
+    The two arguments needed are:
+    df:the DataFrame
+    column:the column from the dataFrame as a string
+    """
+    df[column]=df[column].map(lambda x: int(x))
+    return df   
